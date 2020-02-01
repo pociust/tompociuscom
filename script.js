a20e8b76277e4e8ccd000c0b00fee1bf5a0270da
Tom Pocius <poci14@gmail.com>
2020-01-31 23:06:51 -0600

    changed theme for profile and added landing page

--- script.js
+++ script.js
@@ -1,27 +1,5 @@
-function randomFact(){
-  var tomFactArray = ['Tom was born 09/23/1988', 'Tom has a twin brother named Mike', 'Tom has a doctorate in Physical Therapy', 'Tom has a dog named Evey'];
-  var ri = Math.floor(Math.random() * tomFactArray.length);
-  document.getElementById("factsButton").innerHTML = (tomFactArray[ri]);
-}  
-
-/*var iframe = document.getElementById("iframe");
-var div = document.getElementById("sailing");
-div.innerHTML = iframe.contentWindow.document.getElementById("").innerHTML;*/
-
-var slideIndex = 1;
-showDivs(slideIndex);
-
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
-
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1} 
-  if (n < 1) {slideIndex = x.length} ;
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none"; 
-  }
-  x[slideIndex-1].style.display = "block"; 
-}
+document.getElementById('landing-button-div').addEventListener('click', () => {
+  document
+    .getElementById('section-2')
+    .scrollIntoView({ block: 'start', behavior: 'smooth' });
+});
